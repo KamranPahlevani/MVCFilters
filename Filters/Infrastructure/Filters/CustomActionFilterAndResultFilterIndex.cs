b1a3da69acc16f2d510588011055f09aaae8a0c7
masoudparsa <m.parsanejad@gmail.com>
2016-05-30 14:51:09 +0430

    test

---
+++ Filters/Infrastructure/Filters/CustomActionFilterAndResultFilterIndex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure.Filters
+{
+    public class CustomActionFilterAndResultFilterIndex:ActionFilterAttribute
+    {
+        private Stopwatch timer;
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            timer.Stop();
+            if (filterContext.Exception == null)
+                filterContext.Controller.ViewData.Model = timer.Elapsed.TotalSeconds.ToString();
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            timer = Stopwatch.StartNew();
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext filterContext)
+        {
+            timer.Stop();
+            if (filterContext.Exception == null)
+                filterContext.HttpContext.Response.Write("result elapsed time: " + timer.Elapsed.TotalSeconds.ToString());
+        }
+    }
+}
---
+++ Filters/Infrastructure/Filters/CustomAuthorizeAttribute.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure.Filters
+{
+    public class CustomAuthorizeAttribute:AuthorizeAttribute
+    {
+        private string[] allowedUsers;
+
+        public CustomAuthorizeAttribute(params string[] users)
+        {
+            allowedUsers = users;
+        }
+
+        /// <summary>
+        /// this method use for user Authorize and user Role and user Access Level
+        /// this method dont know any thing about request and relative request objects for example RouteData
+        /// </summary>
+        /// <param name="httpContext"></param>
+        /// <returns></returns>
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool isAuthenticated = (httpContext.Request.IsAuthenticated && allowedUsers.Contains(httpContext.User.Identity.Name, StringComparer.InvariantCultureIgnoreCase) || httpContext.Request.IsLocal);
+            return isAuthenticated;
+        }
+
+        /// <summary>
+        /// this method use for logging and session management
+        /// this method access to request object
+        /// AuthorizationContext is concrete of ControllerContext
+        /// </summary>
+        /// <param name="filterContext"></param>
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            ///if dont set result mvc assume that this action method is authorized
+            ///but if set result mvc prevent action process
+            //filterContext.Result = new HttpUnauthorizedResult();
+
+            base.OnAuthorization(filterContext);
+        }
+
+        protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                UrlHelper urlHelper = new UrlHelper(filterContext.RequestContext);
+                filterContext.Result = new JsonResult()
+                {
+                    Data = new
+                    {
+                        Error = "NotAuthorized",
+                        LogOnUrl = urlHelper.Action("LogOn", "Account")
+                    },
+                    JsonRequestBehavior = JsonRequestBehavior.AllowGet
+                };
+
+
+            }
+            else
+                base.HandleUnauthorizedRequest(filterContext);
+        }
+
+    }
+}
